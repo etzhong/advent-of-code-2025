ab577bf2e5ae79f058a62746846f9d52e724c20d
etzhong <eric@erictzhong.com>
2025-12-05 16:25:54 -0800

    Day 2 template

---
+++ day2/day2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
+/*
+func outputInvalidFromPair (a, b) {
+
+}
+
+
+Key Insights
+    (1) An invalid ID MUST have an even number of digits
+    (2) Given (1), we only need to check the "even-digit subrange" of an invalid range
+    (3)
+
+*/
