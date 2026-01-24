11c0c9b5cf6c1bc496f4ce67627fdef23f1cf94f
etzhong <eric@erictzhong.com>
2026-01-24 15:33:51 -0800

    Day5p1 completion

---
+++ day5/day5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ifstream ifile("input.txt");
+    if (!ifile.is_open()) {
+        std::cerr << "Error opening input file" << std::endl;
+        return 1;
+    }
+    std::string line;
+    std::vector<std::pair<long long, long long>> intervals;
+    std::vector<long long> ids;
+    while (std::getline(ifile, line) && !line.empty())
+        intervals.emplace_back(std::stoll(line.substr(0, line.find('-'))), std::stoll(line.substr(line.find('-')+1)));
+    while (std::getline(ifile, line))
+        ids.emplace_back(std::stoll(line));
+    int numFreshItems = 0;
+    bool isFresh;
+    for (auto const id : ids) {
+        isFresh = false;
+        for (auto const& interval : intervals) {
+            if (interval.first <= id && id <= interval.second) {
+                isFresh = true;
+                break;
+            }
+        }
+        numFreshItems += isFresh;
+    }
+    std::cout << "Number of Fresh Items: " << numFreshItems << std::endl;
+    return 0;
+}
