109c4afabde14427881072cba59f744d6ae32e16
etzhong <eric@erictzhong.com>
2026-01-24 14:44:27 -0800

    Day 4p1

---
+++ day4/day4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+
+bool const can_access(const std::vector<std::string>& map, int row, int col) {
+    bool const onTopEdge = (row == 0);
+    bool const onBotEdge = (row == (map.size()-1));
+    bool const onLeftEdge = (col == 0);
+    bool const onRightEdge = (col == (map.front().size()-1));
+    int numAdjacentRolls = 0;
+    if (!onTopEdge && !onLeftEdge)  numAdjacentRolls += (map[row-1][col-1] == '@');
+    if (!onTopEdge)                 numAdjacentRolls += (map[row-1][col] == '@');
+    if (!onTopEdge && !onRightEdge) numAdjacentRolls += (map[row-1][col+1] == '@');
+    if (!onLeftEdge)                numAdjacentRolls += (map[row][col-1] == '@');
+    if (!onRightEdge)               numAdjacentRolls += (map[row][col+1] == '@');
+    if (!onBotEdge && !onLeftEdge)  numAdjacentRolls += (map[row+1][col-1] == '@');
+    if (!onBotEdge)                 numAdjacentRolls += (map[row+1][col] == '@');
+    if (!onBotEdge && !onRightEdge)  numAdjacentRolls += (map[row+1][col+1] == '@');
+    return (numAdjacentRolls < 4);
+}
+
+int main() {
+    std::vector<std::string> map;
+    std::string row;
+    std::ifstream ifile("input.txt");
+    if (!ifile.is_open()) {
+        std::cerr << "Error opening input file" << std::endl;
+        return 1;
+    }
+    while (std::getline(ifile, row))
+        map.emplace_back(row);
+    int numAccessibleRolls = 0;
+    for (int row=0; row<map.size(); row++)
+        for (int col=0; col<map.front().size(); col++)
+            if (map[row][col] == '@' && can_access(map, row, col))
+                numAccessibleRolls++;
+    std::cout << "Acessible Rolls: " << numAccessibleRolls << std::endl;
+    return 0;
+}
